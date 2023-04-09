1c041001cef6576759a629c0dba9582cc9b9e3d5
WhiteRabbitXXX <130298419+WhiteRabbitXXX@users.noreply.github.com>
2023-04-09 17:48:27 +0300

    Update Testname.cs

--- Testname.cs
+++ Testname.cs
@@ -6,6 +6,12 @@ namespace Testname
     static void TestingName()
       {
        Console.WriteLine("its alive");
+       double b = Math.Exp(1);
+       int x = 14;
+       double n = Math.Pow(10,-x);
+       double ans = Math.Truncate((Math.Round(b, x+1)/n))*n;
+       Console.WriteLine(b);
+       Console.WriteLine(ans);
       }
    }
 }
